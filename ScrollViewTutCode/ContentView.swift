7439c10989144a6a90521c1ed81257ce9c02fe14
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-28 17:38:41 +0200

    .../

--- ScrollViewTutCode/ContentView.swift
+++ ScrollViewTutCode/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ScrollView {
+            LazyVStack {
+                ForEach(0..<10) { index in
+                    ScrollView(.horizontal, showsIndicators: false, content: {
+                        LazyHStack {
+                            ForEach(0..<15) { index in
+                                RoundedRectangle(cornerRadius: 7)
+                                    .fill(Color.white)
+                                    .frame(width: 150, height: 100)
+                                    .shadow(radius: 10)
+                                    .padding()
+                            }
+                            
+                        }
+
+                    })
+                                        //Text("Rectangle :: \(index)")
+                    
+                }
+                
+            }
+        }
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
