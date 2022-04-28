4b66cf582cdf8e34c75a18f43e1dc1d4c9fe5555
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-28 15:50:24 +0200

    Initial Commit

---
+++ ScrollViewTutCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrollViewTutCode
+//
+//  Created by HAL-9001 on 28/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrollViewTutCode/ScrollViewTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewTutCodeApp.swift
+//  ScrollViewTutCode
+//
+//  Created by HAL-9001 on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
